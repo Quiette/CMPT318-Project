c7281e10c0857c5ed8216b2d26058d91f9c4da34
Kirby Rodriguez <kirby122402@gmail.com>
2022-09-29 09:20:34 -0700

    fixing line spacing

--- A1/Assignment1.R
+++ A1/Assignment1.R
@@ -7,12 +7,12 @@ library("lubridate")
 #install.packages("psych")
 library("psych") 
 
-
 #install.packages("Hmisc")
 library("Hmisc")
 
 #install.packages("corrplot")
 library("corrplot")
+
 #install.packages("modeest")
 library("modeest") 
 
@@ -100,8 +100,7 @@ for(dTable in WeekSplitDay){
 # 2 - done by Kirby
 
 table2 <- subset(table, select = -c(Date,Time,WeekdayBool))
-res <- corr.test(table2, y = NULL, use = "complete",method="pearson",adjust="holm", 
-                 alpha=.01)
+res <- corr.test(table2, y = NULL, use = "complete",method="pearson",adjust="holm", alpha=.01)
 corrplot(res$r, type="upper", order="hclust", p.mat = res$P, sig.level = 0.01, addCoef.col = 'black', is.corr = FALSE, insig = "label_sig")
 
 
