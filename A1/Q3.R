d4961a37fd423545c9f897d6f529583e88173584
Nicole Vavrukh <nicole.vavrukh@gmail.com>
2022-09-29 13:52:13 -0700

    added some to start work

--- A1/Q3.R
+++ A1/Q3.R
@@ -10,6 +10,11 @@ library("psych")
 #install.packages("modeest")
 library("modeest") 
 
+library(ggplot2)
+
+# Set working directory to file location
+setwd(dirname(getActiveDocumentContext()$path)) 
+getwd()
 
 dayTimeStart = "09:00:00"
 nightTimeStart = "17:00:00"
@@ -60,6 +65,25 @@ wkendDay_avg <- aggregate(PowerTimeWkendDay$Global_intensity,
                           list(PowerTimeWkendDay$Time), mean)
 wkendNight_avg <- aggregate(PowerTimeWkendNight$Global_intensity, 
           list(PowerTimeWkendNight$Time), mean)
+
+wkendNight_avg$Time = as.POSIXct(wkendNight_avg$Group.1, format = "%H:%M:%S")
+wkendDay_avg$Time = as.POSIXct(wkendDay_avg$Group.1, format = "%H:%M:%S")
+wkdayNight_avg$Time = as.POSIXct(wkdayNight_avg$Group.1, format = "%H:%M:%S")
+wkdayDay_avg$Time = as.POSIXct(wkdayDay_avg$Group.1, format = "%H:%M:%S")
+
+WkEndNightPlot <- plot(x = wkendNight_avg$Time, y = wkendNight_avg$x)
+WkdayNightPlot <-plot(x = wkdayNight_avg$Time, y = wkdayNight_avg$x)
+
+WkEndDayPlot <- plot(x = wkendDay_avg$Time, y = wkendDay_avg$x, type = "l", lty = 1, lwd=0.5, col = "red")
+points(wkdayDay_avg$Time, y = wkdayDay_avg$x, type = "l", lty = 5, lwd=0.5, col = "blue")
+legend("top", legend=c("Weekday", "weekend"),col=c("blue", "red"), cex=0.8,title="Data Legend", text.font=4, lty = 1:1)
+
+
+abline(lm(wkdayDay_avg$x ~ wkdayDay_avg$Time, wkdayDay_avg), col="blue", lwd = 2.0)
+abline(lm(wkendDay_avg$x ~ wkendDay_avg$Time, wkendDay_avg), col= "red", lwd = 2.0)
+legend("topright", legend=c("Weekday Line", "weekend Line"),col=c("blue", "red"), cex=0.8,title="Data Legend", text.font=4, lty = 1:1, lwd = 2.0:2.0)
+
+
 
 View(wkdayDay_avg)
 View(wkdayNight_avg)
